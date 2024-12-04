c5b67f75928e1e9f5381ff2ce498216b6d89badd
ryanpompeo <ryanpompeo05@gmail.com>
2024-12-04 00:43:06 -0300

    Implemented Board class e modified Piece class

---
+++ src/boardgame/Board.java
@@ -0,0 +1,30 @@
+package boardgame;
+
+public class Board {
+
+	private int linhas;
+	private int colunas;
+	private Piece[][] pieces;
+	
+	public Board(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		pieces = new Piece[linhas][colunas];
+	}
+	
+	public int getLinhas() {
+		return linhas;
+	}
+	
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+	
+	public int getColunas() {
+		return colunas;
+	}
+	
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+}
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,4 +1,19 @@
 package boardgame;
 
 public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	
+	/*
+	Exclusão do setBoard impossibilita alteração do tabuleiro
+	 */
+	protected Board getBoard() {
+		return board;
+	}
 }
