53dc2a9ca7ba00663e728506c7a99683189c529e
ryanpompeo <ryanpompeo05@gmail.com>
2024-12-05 06:13:46 -0300

    add possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,8 +1,11 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
+
+import java.awt.*;
 
 public class King extends ChessPiece {
 	public King(Board board, Color color) {
@@ -14,10 +17,66 @@ public class King extends ChessPiece {
 		return "K";
 	}
 	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
 	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matriz = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+		
+		Position p = new Position(0, 0);
+		
+		//Verificação da casa a cima da peça
+		p.setValues(position.getLinha() - 1, position.getColuna());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa abaixo da peça
+		p.setValues(position.getLinha() + 1, position.getColuna());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa a esqueda da peça
+		p.setValues(position.getLinha(), position.getColuna() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa a direita da peça
+		p.setValues(position.getLinha(), position.getColuna() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa da diagonal superior esquerda da peça
+		p.setValues(position.getLinha() - 1, position.getColuna() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa da diagonal superior direita da peça
+		p.setValues(position.getLinha() - 1, position.getColuna() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa da diagonal inferior esqueda da peça
+		p.setValues(position.getLinha() + 1, position.getColuna() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificação da casa da diagonal inferior direita da peça
+		p.setValues(position.getLinha() + 1, position.getColuna() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		
 		return matriz;
 	}
 }
