896f88b4df1a0422225d9dce7f7478a53f9e1255
ryanpompeo <ryanpompeo05@gmail.com>
2024-12-07 23:09:00 -0300

    add bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,68 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+	
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+		
+		Position p = new Position(0, 0);
+		
+		//Verificar diagonal esquerda superior da peça
+		p.setValues(position.getLinha() - 1, position.getColuna() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentePiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificar casas diagonal superior direita
+		p.setValues(position.getLinha() - 1, position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentePiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//Verificar diagonal inferior direita
+		p.setValues(position.getLinha() + 1, position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentePiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		//Verificar diagonal inferior esquerda
+		p.setValues(position.getLinha() + 1, position.getColuna() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentePiece(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		
+		return matriz;
+	}
+	
+}
