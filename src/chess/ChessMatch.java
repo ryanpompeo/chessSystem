1d391685b655963c0931705ab09b43caa48dc5e1
ryanpompeo <ryanpompeo05@gmail.com>
2024-12-07 22:28:11 -0300

    add checkmate

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 	private boolean check;
+	private boolean CheckMate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -32,14 +33,17 @@ public class ChessMatch {
 		return turn;
 	}
 	
+	public boolean getCheckMate() {
+		return CheckMate;
+	}
+	
 	public Color getCurrentPlayer() {
 		return currentPlayer;
 	}
 	
-	public boolean getCheck(){
+	public boolean getCheck() {
 		return check;
 	}
-	
 	
 	
 	public ChessPiece[][] getPieces() {
@@ -73,7 +77,11 @@ public class ChessMatch {
 		
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		
-		nextTurn();
+		if (testCheckMate(opponent(currentPlayer))) {
+			CheckMate = true;
+		} else {
+			nextTurn();
+		}
 		return (ChessPiece) capturedPiece;
 	}
 	
@@ -156,9 +164,36 @@ public class ChessMatch {
 			
 		}
 		return false;
-		
+	}
+	
+	private boolean testCheckMate(Color color) {
+		if (!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color).collect(Collectors.toList());
+		for (Piece p : list) {
+			boolean[][] matriz = p.possibleMoves();
+			for (int i = 0; i < board.getLinhas(); i++) {
+				for (int j = 0; j < board.getColunas(); j++) {
+					if (matriz[i][j]) {
+						Position source = ((ChessPiece) p).getChessPosition().toPosition();
+						Position target = new Position(i, j);
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						if (!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+			
+			
+		}
+		return true;
 		
 	}
+	
 	
 	private void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
